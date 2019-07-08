da576969629b7ccd0bcac86bfd8d76b9b64049ba
kingBook <412126604@qq.com>
2019-07-08 21:37:50 +0800

    update 2019-07-07 21:37

---
+++ Assets/ProjectImporter/LayerMask2.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct LayerMask2{
+		public int value;
+
+		//需要改
+		public static int GetMask(params string[] layerNames){
+			return LayerMask.GetMask(layerNames);
+		}
+
+		//需要改
+		public static string LayerToName(int layer){
+			return LayerMask.LayerToName(layer);
+		}
+
+		//需要改
+		public static int NameToLayer(string layerName){
+			return LayerMask.NameToLayer(layerName);
+		}
+
+		public static implicit operator int(LayerMask2 mask){
+			return mask.value;
+		}
+		public static implicit operator LayerMask2(int intVal){
+			LayerMask2 mask=new LayerMask2();
+			mask.value=intVal;
+			return mask;
+		}
+
+		public static implicit operator LayerMask2(LayerMask mask){
+			LayerMask2 mask2=new LayerMask2();
+			mask2.value=mask.value;
+			return mask2;
+		}
+		public static implicit operator LayerMask(LayerMask2 mask2){
+			LayerMask mask=new LayerMask();
+			mask.value=mask2.value;
+			return mask;
+		}
+}
---
+++ Assets/ProjectImporter/SortingLayer2.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public struct SortingLayer2{
+	private static SortingLayer[] _layers;
+
+	//需要改
+	public static SortingLayer[] layers{
+		get => _layers;
+	}
+
+	public int id { get; }
+		
+		public string name { get; }
+		public int value { get; }
+		//需要改
+		public static int GetLayerValueFromID(int id){
+			return SortingLayer.GetLayerValueFromID(id);
+		}
+		//需要改
+		public static int GetLayerValueFromName(string name){
+			return SortingLayer.GetLayerValueFromName(name);
+		}
+		//需要改
+		public static string IDToName(int id){
+			return SortingLayer.IDToName(id);
+		}
+		//需要改
+		public static bool IsValid(int id){
+			return SortingLayer.IsValid(id);
+		}
+		//需要改
+		public static int NameToID(string name){
+			return SortingLayer.NameToID(name);
+		}
+}
