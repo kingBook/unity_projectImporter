ebd94469ca361784fa784abe833e53fe6b3af4b3
kingBook <412126604@qq.com>
2019-09-28 23:33:24 +0800

    update 2019-09-29 23:33

--- Assets/ProjectImporter/runtime/Test.cs
+++ Assets/ProjectImporter/runtime/Test.cs
@@ -4,37 +4,20 @@ using System.Text.RegularExpressions;
 using UnityProjectImporter;
 
 public class Test:MonoBehaviour{
-	public Camera cam;
+	public Camera camera;
 	private void Start() {
 		
 	}
 
-	/*private void Update(){
-		if(Input.GetMouseButtonDown(0)){
-			var pos=Input.mousePosition;
-			var camera=Camera.main;
-			pos.z=camera.nearClipPlane;
-			pos=camera.ScreenToWorldPoint(pos);
-			transform.position=pos;
-		}
-	}*/
-	void OnGUI()
-    {
-        Vector3 point = new Vector3();
-        Event   currentEvent = Event.current;
-        Vector2 mousePos = new Vector2();
+	private void Update(){
+		var pos=Input.mousePosition;
+		//pos.z=cam.transform.position.z+10;
+		pos.z=11-camera.transform.position.z;
+		pos=camera.ScreenToWorldPoint(pos);
+		Debug.Log(camera.transform.position.z+","+camera.nearClipPlane+","+pos.z);
+		transform.position=pos;
 
-        // Get the mouse position from Event.
-        // Note that the y position from Event is inverted.
-        mousePos.x = currentEvent.mousePosition.x;
-        mousePos.y = cam.pixelHeight - currentEvent.mousePosition.y;
 
-        point = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.nearClipPlane));
-		transform.position=point;
-        GUILayout.BeginArea(new Rect(20, 20, 250, 120));
-        GUILayout.Label("Screen pixels: " + cam.pixelWidth + ":" + cam.pixelHeight);
-        GUILayout.Label("Mouse position: " + mousePos);
-        GUILayout.Label("World position: " + point.ToString("F3"));
-        GUILayout.EndArea();
-    }
+		transform.rotation=camera.transform.rotation;
+	}
 }
