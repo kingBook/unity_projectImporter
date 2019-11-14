8662cb7c1869ea5edfeccae5d7742486bb1832ef
kingBook <412126604@qq.com>
2019-11-14 22:23:50 +0800

    update 2019-11-14 22:23

--- Assets/UnityTools/Scripts/Editor/CSharpObfuscator/CSharpMethod.cs
+++ Assets/UnityTools/Scripts/Editor/CSharpObfuscator/CSharpMethod.cs
@@ -1,9 +1,34 @@
 ﻿namespace UnityTools{
 	using UnityEngine;
 	using System.Collections;
-
+	/// <summary>
+	/// 方法
+	/// </summary>
 	public struct CSharpMethod{
+		
+		/// <summary>
+		/// 所在的命名空间
+		/// </summary>
+		public CSharpNameSpace nameSpace;
 
+		/// <summary>
+		/// 返回类型
+		/// </summary>
+		public IString returnType;
+
+        /// <summary>
+		/// <para>单词+尖括号/单词</para>
+		/// <para><see cref="Segment"/>/<see cref="WordAngleBrackets"/></para>
+		/// <para>如："xxx&lt;...&gt;","xxx"</para>
+		/// </summary>
+		public IString name;
+
+		public object parameters;
+
+		/// <summary>
+		/// 泛型约束列表
+		/// </summary>
+		public CSharpGenericConstraint[] genericConstraints;
 		
 	}
 }
---
+++ Assets/UnityTools/Scripts/Editor/CSharpObfuscator/CSharpProperty.cs
@@ -0,0 +1,11 @@
+﻿namespace UnityTools{
+	using UnityEngine;
+	using System.Collections;
+
+	public struct CSharpProperty{
+
+		public IString type;
+
+		public Segment name;
+	}
+}
